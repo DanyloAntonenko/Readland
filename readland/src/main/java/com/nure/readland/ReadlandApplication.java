0c309bb9e549b37e307c76c0bf380b54bc1065d1
DanyloAntonenko <danylo.antonenko@nure.ua>
2020-10-23 17:22:24 +0300

    Initial commit

---
+++ readland/src/main/java/com/nure/readland/ReadlandApplication.java
@@ -0,0 +1,13 @@
+package com.nure.readland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadlandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadlandApplication.class, args);
+	}
+
+}
---
+++ readland/src/test/java/com/nure/readland/ReadlandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nure.readland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadlandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
