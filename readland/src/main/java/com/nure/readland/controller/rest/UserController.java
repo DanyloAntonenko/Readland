82a425bc0112fb05bd3a167b1004a28a5abcbed6
DanyloAntonenko <danylo.antonenko@nure.ua>
2020-12-28 17:07:58 +0200

    UserController implemented

--- readland/src/main/java/com/nure/readland/controller/rest/UserController.java
+++ readland/src/main/java/com/nure/readland/controller/rest/UserController.java
@@ -5,39 +5,41 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nure.readland.controller.Constant;
 import com.nure.readland.model.Book;
 import com.nure.readland.model.User;
+import com.nure.readland.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(Constant.API_USER)
 public class UserController {
+
+	@Autowired
+	UserService userService;
+
 	@GetMapping()
 	protected Iterable<User> getAllUsers(){
-		// TODO: 26.12.2020 need dao
-		return null;
+		return userService.findAll();
 	}
 
 	@GetMapping("{id}")
 	protected User getUserById(@PathVariable String id){
-		// TODO: 26.12.2020 need dao
-		return null;
+		return userService.findById(Long.parseLong(id));
 	}
 
 	@PutMapping()
-	protected User addUser(@RequestBody String body){
-		// TODO: 26.12.2020 need dao
-		return null;
+	protected User addUser(@RequestBody String body) throws JsonProcessingException{
+		User parsed = new ObjectMapper().readValue(body, User.class);
+		return userService.create(parsed);
 	}
 
 	@PatchMapping("{id}")
 	protected User updateUser(@PathVariable String id, @RequestBody String body) throws JsonProcessingException {
-		User result =
-				new ObjectMapper().readValue(body, User.class);
-		// TODO: 26.12.2020 need dao
-		return null;
+		User result = new ObjectMapper().readValue(body, User.class);
+		return userService.update(result);
 	}
 
 	@DeleteMapping("{id}")
 	protected void deleteUser(@PathVariable String id){
-
+		userService.delete(userService.findById(Long.valueOf(id)));
 	}
 }
