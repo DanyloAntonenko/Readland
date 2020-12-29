ad2b96fb44db27e23d740bc1e917217459c96516
Stardusted1 <refineanswer@gmail.com>
2020-12-29 21:19:34 +0200

    .gitignore

--- readland/src/main/java/com/nure/readland/ReadlandApplication.java
+++ readland/src/main/java/com/nure/readland/ReadlandApplication.java
@@ -10,4 +10,6 @@ public class ReadlandApplication {
 		SpringApplication.run(ReadlandApplication.class, args);
 	}
 
+	// TODO: 29.12.2020  git rm --cached C:/path_to_repo/Readland/readland/src/main/resources/hibernate.cfg.xml
+	// git rm --cached C:/path_to_repo/Readland/readland/src/main/resources/application.properties
 }
